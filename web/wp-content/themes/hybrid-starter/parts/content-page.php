0031a34ded9c9d3d0e9d4a39e44207716ccf0cc3
ext2txe <ext2txe@gmail.com>
2026-04-12 12:29:59 +0200

    initial commit

---
+++ web/wp-content/themes/hybrid-starter/parts/content-page.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Page content.
+ *
+ * @package Hybrid_Starter
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class( 'entry' ); ?>>
+	<header class="entry-header">
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+	</header>
+
+	<div class="entry-content">
+		<?php
+		the_content();
+		wp_link_pages();
+		?>
+	</div>
+</article>
+
